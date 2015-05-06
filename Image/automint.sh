5ebc52879adf5319d7694000a7db1b5f8fe806c7
vitor251093 <vitor251093@users.noreply.github.com>
2015-05-06 07:42:08 -0300

    Update automint.sh

--- Image/automint.sh
+++ Image/automint.sh
@@ -6,7 +6,6 @@ activateBURG='YES'
 activateGuest='NO'
 activateMySQL='NO'
 activateTheme='YES'
-activateWindows='NO'
 activateAutoShutdown='NO'
 
 while [ "$1" != "" ]; do
@@ -29,8 +28,6 @@ while [ "$1" != "" ]; do
                                 ;;
         -nB | --no-burg )       activateBURG='NO'
                                 ;;
-        -w | -windows )         activateWindows='YES'
-                                ;;
         -h | -s | -shutdown )   activateAutoShutdown='YES'
                                 ;;
         * )                     usage
@@ -39,8 +36,18 @@ while [ "$1" != "" ]; do
     shift
 done
 
-red='\033[1;31m'
-NC='\033[0m'
+function getTextWithGramarFromWebsite() 
+{
+    echo $(wget --quiet -O - $2 | grep -o $1 | head -1)
+}
+
+function getSourceForgeDirectLinkWithLink()
+{
+    local SrcLink="${1/sourceforge.net/$2.sourceforge.net}"
+    SrcLink="${SrcLink/projects/project}"
+    SrcLink="${SrcLink/files\//}"
+    echo "http://$SrcLink"
+}
 
 if [[ $activateBURG == "YES" ]]; then
 	echo -e "${red}Type BURG password:${NC}"
@@ -79,10 +86,6 @@ if [[ $activateMySQL == "YES" ]]; then
 	done
 fi
 
-
-#The following folder is the online path where you will download these files: 
-#checkWeb.sh, index.html, logo.png, offline.png, online.png and posimage.sh
-LoginFilesFolder="http://university.website/~your_account/Image_files"
 Message1="educationalMint was created by VitorMM,"
 Message2="but it's Creative Commons, so it's yours."
 
@@ -91,56 +94,6 @@ LinuxVersion=$(lsb_release -d -s)
 LinuxArchitecture=$(getconf LONG_BIT)
 Linux="$LinuxVersion $LinuxArchitecture-bits"
 echo -e "${red}Linux: $Linux${NC}"
-
-if [[ $activateWindows == "YES" ]]; then
-	echo -e "${red}Getting Windows partition...${NC}"
-	IFS='\n'
-	partitions=$(cat /proc/partitions)
-	gramar="[0-9]*[^+]sda[1-9]"
-	partitions=$(echo $partitions | grep -o $gramar)
-	gramar="[0-9][0-9][0-9]*"
-	partitionSize=$(echo $partitions | grep -o $gramar)
-	partitionSize=$(echo $partitionSize | sort -k1,1n)
-	partitionSize=$(echo $partitionSize | tail -1)
-	gramar="$partitionSize[^+]sda[1-9]"
-	partitions=$(echo $partitions | grep -o $gramar)
-	gramar="sda[1-9]"
-	windowsPartition=$(echo $partitions | grep -o $gramar)
-	echo -e "${red}Windows partition found: $windowsPartition${NC}"
-	IFS=$'\n'
-	
-	echo -e "${red}Getting Windows version...${NC}"
-	apt-get install -y reglookup
-	mount /dev/$windowsPartition /mnt
-	cd /mnt/Windows/System32/config/
-	if [ -f "software" ]; then
-  		reg=$(reglookup -p "Microsoft/Windows NT/CurrentVersion/ProductName" software)
-	else
-  		reg=$(reglookup -p "Microsoft/Windows NT/CurrentVersion/ProductName" SOFTWARE)
-	fi
-	WindowsVersion=$(echo $reg | grep -o 'SZ,[^?]*,')
-	WindowsVersion=$(echo $WindowsVersion | grep -o ',[^,]*')
-	WindowsVersion=$(echo $WindowsVersion | grep -o '[^,]*')
-	if [ -f "system" ]; then
-  		reg=$(reglookup -p "ControlSet001/Control/Session Manager/Environment/PROCESSOR_ARCHITECTURE" system)
-	else 
-  		reg=$(reglookup -p "ControlSet001/Control/Session Manager/Environment/PROCESSOR_ARCHITECTURE" SYSTEM)
-	fi
-	WindowsArchitecture=$(echo $reg | grep -o 'SZ,[^?]*,')
-	WindowsArchitecture=$(echo $WindowsArchitecture | grep -o ',[^,]*')
-	WindowsArchitecture=$(echo $WindowsArchitecture | grep -o '[^, ]*')
-	if [[ $WindowsArchitecture == "x86" ]]; then
-  		WindowsArchitecture="32-bits"
-	else
-  		WindowsArchitecture="64-bits"
-	fi
-	Windows="Microsoft $WindowsVersion$WindowsArchitecture"
-	cd ~/
-	apt-get remove -y reglookup
-	umount /mnt
-	echo -e "${red}Windows: $Windows${NC}"
-fi
-
 
 echo -e "${red}Updating bash to avoid vulnerabilities...${NC}"
 echo -e "${red}-> Updating repositories list...${NC}"
